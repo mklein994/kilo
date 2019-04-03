e203665142e471f7802facbeee684307365e9e0c
Matthew Klein <mklein994@gmail.com>
2019-04-03 09:32:46 -0500

    Use the correct 'c flag' when disabling flags
    This fixes the problem where it wouldn't quit even if ^q was typed.

--- kilo.c
+++ kilo.c
@@ -37,9 +37,9 @@ void enableRawMode() {
   if (tcgetattr(STDIN_FILENO, &raw) == -1)
     die("tcgetattr");
 
-  raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-  raw.c_lflag &= ~(OPOST);
-  raw.c_lflag |= (CS8);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
