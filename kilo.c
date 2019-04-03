1f9de2917f2691b8533f78cadf662e20fc4d5b66
Matthew Klein <mklein994@gmail.com>
2019-04-03 10:04:26 -0500

    Store screen size in editorConfig struct

--- kilo.c
+++ kilo.c
@@ -15,6 +15,8 @@
 /*** data ***/
 
 struct editorConfig {
+  int screenrows;
+  int screencols;
   struct termios orig_termios;
 };
 
@@ -112,8 +114,14 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+    die("getWindowSize");
+}
+
 int main() {
   enableRawMode();
+  initEditor();
 
   while (1) {
     editorRefreshScreen();
