d1759f598fe915d23b887b90af1cad28fbcb6fa4
Matthew Klein <mklein994@gmail.com>
2019-04-04 11:12:00 -0500

    Create an enum to hold syntax types in a char array

--- kilo.c
+++ kilo.c
@@ -38,6 +38,11 @@ enum editorKey {
   PAGE_DOWN,
 };
 
+enum editorHighlight {
+  HL_NORMAL = 0,
+  HL_NUMBER,
+};
+
 /*** data ***/
 
 typedef struct erow {
@@ -45,6 +50,7 @@ typedef struct erow {
   int rsize;
   char *chars;
   char *render;
+  unsigned char *hl;
 } erow;
 
 struct editorConfig {
@@ -275,6 +281,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 
   E.row[at].rsize = 0;
   E.row[at].render = NULL;
+  E.row[at].hl = NULL;
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
@@ -284,6 +291,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 void editorFreeRow(erow *row) {
   free(row->render);
   free(row->chars);
+  free(row->hl);
 }
 
 void editorDelRow(int at) {
